0b2987a941911d993a387bb38bdc61edcc634ec1
PinguDash <132189736+PinguDash@users.noreply.github.com>
2023-09-25 09:31:13 +0200

    Overall Changes to scripts.js
    can now toggle credits, added comments and line breaks

--- scripts.js
+++ scripts.js
@@ -1,4 +1,11 @@
 function hideDisclaimers() {
+  
+  /*Removes the disclaimer about how characters are included*/
   const chardisclaimer = document.getElementById("disclaimerone")
   chardisclaimer.remove()
+  
+  /*Removes the credits at the very bottom*/
+  const credits = document.getElementById("credits")
+  credits.remove()
+  
 }
