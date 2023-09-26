386849efbc0f7225e551061e724f9447ea0f9dda
PinguDash <132189736+PinguDash@users.noreply.github.com>
2023-09-26 20:23:10 +0200

    Added comments to scripts.js

--- scripts.js
+++ scripts.js
@@ -1,16 +1,21 @@
 function toggleCredits() {
+
+    /*Gets credits const*/
     const credits = document.getElementById("credits");
     const computedStyle = window.getComputedStyle(credits);
 
+    /*Toggles visibility*/
     if (computedStyle.visibility === "hidden") {
         credits.style.visibility = "visible";
     } else {
         credits.style.visibility = "hidden";
     }
 
+    /*- disclaimer const -*/
     const disclaimerone = document.getElementById("disclaimerone");
     const computedStyleone = window.getComputedStyle(disclaimerone);
 
+    /*Same here*/
     if (computedStyleone.visibility === "hidden") {
         disclaimerone.style.visibility = "visible";
     } else {
